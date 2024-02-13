2c0ee5a3b04edd0967391b2d2140bd8d684caa56
wpengjie0214 <159807936+wpengjie0214@users.noreply.github.com>
2024-02-13 16:33:06 +0800

    Update config.ts
    修改了访问密码权限

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -13,9 +13,9 @@ export default defineUserConfig({
     encrypt: {
       config: {
         // 这会加密整个 guide 目录，并且两个密码都是可用的
-        "/guide/": ["1234", "5678"],
+        "/_posts/": ["1234", "5678"],
         // 这只会加密 config/page.html
-       // "/config/page.html": "1234",
+       "/family/Diet.html": "1234",
       },
     },
   }),
