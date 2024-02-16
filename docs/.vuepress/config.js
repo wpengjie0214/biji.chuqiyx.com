7d031b4fa90b812e5d8864711df430bd89ef3357
wpengjie0214 <wpengjie0214@163.com>
2024-02-16 18:47:24 +0800

    修改插件
    1112

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,16 @@
 import { copyrightPlugin } from '@vuepress/plugin-copyright'
 
-export default {
+module.exports = {
   plugins: [
-    copyrightPlugin({
-     	disableCopy: true,
-    }),
+    [
+      copyrightPlugin,
+      {
+        global: true, // 让插件全局生效
+        triggerLength: 150, // 设置触发追加版权信息的长度阈值
+        author: "Your Name", // 设置全局作者信息
+        license: "CC BY-NC-SA 4.0", // 设置全局协议信息
+        // 可以添加其他选项进行定制
+      },
+    ],
   ],
-}
+};
