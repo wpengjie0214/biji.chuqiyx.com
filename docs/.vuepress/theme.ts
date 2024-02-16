5a1364a89901588da7e7ce345f8e6f870572c46c
wpengjie0214 <wpengjie0214@163.com>
2024-02-16 19:11:30 +0800

    修改2222
    111

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -29,7 +29,7 @@ export default hopeTheme({
     },
 
   // 是否全局启用路径导航
-  breadcrumb: true,
+  breadcrumb: false,
 
   // 页面元数据：贡献者，最后修改时间，编辑链接
   contributors: false,
@@ -95,7 +95,7 @@ export default hopeTheme({
   },
 
   // 隐藏打印按钮
-  print: false,
+  print: true,
 
 
 
