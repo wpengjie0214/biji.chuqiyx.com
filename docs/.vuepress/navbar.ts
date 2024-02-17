64eb30a288854ff2a0292e0e3b328e2dd95eb53d
wpengjie0214 <wpengjie0214@163.com>
2024-02-17 21:58:24 +0800

    修改内容
    1112

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -3,10 +3,10 @@ import { navbar } from "vuepress-theme-hope";
 // 精选图标：https://theme-hope.vuejs.press/zh/guide/interface/icon.html#iconfont-%E7%B2%BE%E9%80%89%E5%9B%BE%E6%A0%87
 //专题话题的路径需在尾部添加 /，否则有可能出现链接错误。比如下方「生活」中的 baby/
 export default navbar([
-  { text: "绘画教程", icon: "blog", link: "https://www.chuqiyx.com/" },
+  { text: "绘画教程", icon: "home", link: "https://www.chuqiyx.com/" },
   {
     text: "工具资料",
-    icon: "app",
+    icon: "type",
     prefix: "/",
     children: [
       {
@@ -46,7 +46,7 @@ export default navbar([
   },
   {
     text: "关于我们",
-    icon: "tool",
+    icon: "mark",
     children: [
       {
         text: "官方网站",
